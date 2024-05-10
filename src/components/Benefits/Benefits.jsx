9983a2c1b8fce2978696cb95807a7725776e20ee
danyalahmad2000 <danigurrero.dg@gmail.com>
2024-05-10 19:44:12 +0500

    Client Requirements Done

---
+++ src/components/Benefits/Benefits.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import america from "../../assets/images/america.jpg";
+import france from "../../assets/images/france.jpg";
+import italy from "../../assets/images/italy.jpg";
+import london from "../../assets/images/london.jpg";
+
+const Benefits = () => {
+  return (
+    <>
+      <div className="w-full max-h-[600px] rounded-[20px] mb-[30px] ">
+        <div className="bg-[#ECF1F7] size-[300px] rounded-full mx-auto flex justify-center items-center hover:bg-blue-400">
+          <p className="text-[32px] font-[800] text-center">
+            No Tenancy Agreements
+          </p>
+        </div>
+        <p className="text-[24px] text-center font-[800] mt-10">
+          No Hassle for Landlords
+        </p>
+        <p className="text-[16px] text-center font-[500] mt-2">
+          Landlords don't have to enter tenancy agreements or burden the risks
+        </p>
+      </div>
+
+      <div className="w-full max-h-[600px] rounded-[20px] mb-[30px] ">
+        <div className="bg-[#ECF1F7] size-[400px] rounded-full mx-auto flex justify-center items-center hover:bg-blue-400">
+          <p className="text-[32px] font-[800] text-center">
+            Cut out expensive Agency and Management Fees
+          </p>
+        </div>
+        <p className="text-[24px] text-center font-[800] mt-10">
+          More Money on the Table
+        </p>
+        <p className="text-[16px] text-center font-[500] mt-2">
+          Assets Ally takes a small 6% fee for Brokering the deal and allows you
+          to bypass traditional 10-15% fees.
+        </p>
+      </div>
+
+      <div className="w-full max-h-[600px] rounded-[20px] mb-[30px] ">
+        <div className="bg-[#ECF1F7] size-[300px] rounded-full mx-auto flex justify-center items-center hover:bg-blue-400">
+          <p className="text-[32px] font-[800] text-center">
+            Tax & Legal Benefits
+          </p>
+        </div>
+        <p className="text-[24px] text-center font-[800] mt-10">
+          More Protection, Less Problems
+        </p>
+        <p className="text-[16px] text-center font-[500] mt-2">
+          Business Deals instead of tenancy agreements means more protection and
+          control for landlords. As well as more tax deductables
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Benefits;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,44 +2,40 @@ import React from "react";
 import Tabs from "../components/Search/Tabs";
 import heroImg from "../assets/images/hero-img.png";
 import heroImg2 from "../assets/images/hero-img-2.png";
-import CountryCards from "../components/CountryCards/CountryCards";
+import Benefits from "../components/Benefits/Benefits";
 
 const Home = () => {
   return (
-    <div className="container mx-auto mt-[120px]">
-      <div className="bg-[#ECF1F7] relative h-screen sm:h-[800px] lg:max-h-[600px] rounded-[70px] flex flex-col lg:flex-row justify-between mb-[40px] overflow-hidden">
-        <div className="px-2 py-2 lg:pl-7 xl:pl-32 lg:pt-32 absolute lg:relative z-10 container lg:w-1/2">
-          <div className="w-full lg:max-w-[500px]">
+    <div className="container mx-auto mt-[120px] relative">
+      <img
+        className="size-[200px] lg:size-[600px] absolute top-0 right-0 bottom-0 left-0 z-[-1] lg:hidden"
+        src={heroImg}
+        alt=""
+      />
+      <div className="bg-[#ECF1F7] relative h-screen sm:h-[800px] lg:max-h-[600px] rounded-[70px] flex flex-col justify-between mb-[40px] overflow-hidden">
+        <div className="px-2 py-2 lg:pt-32 absolute lg:relative z-10 container lg:w-2/3 ">
+          <div className="w-full lg:max-w-[900px] text-center mx-auto">
             <h1 className="heading mt-[240px] lg:mt-0 mb-[10px] lg:mb-[40px] text-black lg:text-black">
-              Easy way to find a perfect property
+              Assets Ally
             </h1>
-            <p className="text__para mb-[10px] lg:mb-[40px]]">
-              We provide a complete service for the sale, purchase or rental of
-              real estate
+            <p className="text__para mb-[10px] lg:mb-[40px]">
+              The market place that matches Landlords with Property Managers
             </p>
           </div>
-          <div className="mt-[40px] lg:relative flex justify-center lg:block">
+          <div className="mt-[40px] lg:relative flex justify-center lg:block ml-12">
             <Tabs />
           </div>
         </div>
-        <div className="lg:w-1/2">
-          <img
-            className="size-[200px] lg:size-[600px] absolute bottom-[-15] right-[240px] lg:right-0 md:right-0 lg:mt-[100px]"
-            src={heroImg}
-            alt=""
-          />
-        </div>
       </div>
-
       <div className="container flex flex-col my-[100px]">
         <div className="w-full mx-auto lg:w-[600px] text-center mb-[40px]">
           <h1 className="heading text-[44px] leading-[55px]">
-            We are available in many well known countries
+            Automate the discovery of your next deal
           </h1>
         </div>
         <div className="flex justify-center">
-          <div className="max-w-full pl-5 gap-10 mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-            <CountryCards />
+          <div className="max-w-full pl-5 gap-20 mt-8 flex flex-row">
+            <Benefits />
           </div>
         </div>
       </div>
